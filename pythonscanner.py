e140a713003a84549cb3c986162d5e8fe43b47f5
AKHILNS28 <akhilns959@gmail.com>
2026-02-07 04:02:47 +0530

    Added a case for TCP Port Scan

--- pythonscanner.py
+++ pythonscanner.py
@@ -1,11 +1,17 @@
 import sys
-import pingsweep
+import socket
+import pingsweep,tcpscanner
 
 print('.....Welcome to Python Network Scanner.....')
 print("1.Ping Sweep")
+print("2.TCP Connect Scan")
 
 choice=int(input("Enter your choice:"))
 
-match(choice):
+match choice:
     case 1:
-        pingsweep.pingsweep(sys.argv[1])
+        addr=input("Enter the network address:")
+        pingsweep.pingsweep(addr)
+    case 2:
+        hostname=input("Enter hostname or ip address:")
+        tcpscanner.scanner(socket.gethostbyname(hostname))
