bdf18a10c1f8dea3aaddd4e9a7c2c86065f6c0ba
AKHILNS28 <akhilns959@gmail.com>
2026-02-08 02:52:36 +0530

    Added more switch cases

--- pythonscanner.py
+++ pythonscanner.py
@@ -6,12 +6,21 @@ print('.....Welcome to Python Network Scanner.....')
 print("1.Ping Sweep")
 print("2.TCP Connect Scan")
 
-choice=int(input("Enter your choice:"))
+try:
+    choice=int(input("Enter your choice:"))
+except ValueError:
+    print('Invalid Choice')
+    sys.exit()
 
 match choice:
     case 1:
         addr=input("Enter the network address:")
         pingsweep.pingsweep(addr)
     case 2:
-        hostname=input("Enter hostname or ip address:")
-        tcpscanner.scanner(socket.gethostbyname(hostname))
+        try:
+            hostname=input("Enter hostname or ip address:")
+            tcpscanner.scanner(socket.gethostbyname(hostname))
+        except socket.gaierror:
+            print('Invalid hostname')
+    case _:
+        print('Invlid choice')
