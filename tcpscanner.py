e3a681444ef60f57639361a16d80e5b8d509cf42
AKHILNS28 <akhilns959@gmail.com>
2026-02-08 02:51:53 +0530

    Added MultiThreading for port scan

--- tcpscanner.py
+++ tcpscanner.py
@@ -1,17 +1,23 @@
 import socket
+from concurrent.futures import ThreadPoolExecutor
+
+def scan(args):
+    ip,port=args
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(0.5)
+    try:
+        sock.connect((ip, port))
+        print(f"[+] {ip:<15} : {port:<5} OPEN")
+    except ConnectionRefusedError:
+        pass
+    except socket.timeout:
+        print(f"[+] {ip:<15} : {port:<5} FILTERED")
+    except OSError:
+        pass
+    finally:
+        sock.close()
 
 def scanner(ip):
-    for i in range(1, 1025):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(0.5)
-        try:
-            sock.connect((ip, i))
-            print(f"Port {i} is OPEN")
-        except ConnectionRefusedError:
-            pass
-        except socket.timeout:
-            print(f"Port {i} is FILTERED")
-        except OSError:
-            pass
-        finally:
-            sock.close()
+    with ThreadPoolExecutor(max_workers=50) as pool:
+        tasks=[(ip,port) for port in range(1,1025)]
+        pool.map(scan,tasks)
